46096e0fe36819f5ef5fed614f9d03e1d56a9540
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2023-02-19 12:56:31 +0100

    reset player on spawn

--- src/main/java/me/verya/bedwars/game/component/Spawn.java
+++ src/main/java/me/verya/bedwars/game/component/Spawn.java
@@ -2,26 +2,36 @@ package me.verya.bedwars.game.component;
 
 import me.verya.bedwars.game.TeleporterLogic;
 import me.verya.bedwars.game.behavior.ClaimManager;
+import me.verya.bedwars.game.event.BedwarsEvents;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.GameMode;
 import xyz.nucleoid.map_templates.BlockBounds;
 import xyz.nucleoid.plasmid.game.GameActivity;
 import xyz.nucleoid.plasmid.game.common.team.GameTeam;
 
 public class Spawn {
-    BlockBounds bounds;
-    GameTeam team;
-    ServerWorld world;
+    final BlockBounds bounds;
+    final GameTeam team;
+    final ServerWorld world;
+    final GameActivity activity;
     public Spawn(BlockBounds bounds, ClaimManager claim, GameTeam team, ServerWorld world, GameActivity activity)
     {
         this.bounds = bounds;
         this.team = team;
         this.world = world;
+        this.activity = activity;
 
         claim.addRegion(this.bounds);
     }
-    public void spawnPlayer(ServerPlayerEntity entity)
+    public void spawnPlayer(ServerPlayerEntity player)
     {
-        TeleporterLogic.spawnPlayer(entity, bounds.center(), world);
+        player.changeGameMode(GameMode.SURVIVAL);
+        player.setVelocity(Vec3d.ZERO);
+        player.fallDistance = 0.0f;
+        player.setHealth(20.0f);
+        TeleporterLogic.spawnPlayer(player, bounds.center(), world);
+        activity.invoker(BedwarsEvents.PLAYER_RESPAWN).onRespawn(player);
     }
 }
