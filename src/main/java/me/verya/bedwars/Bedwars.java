c5c772fdf2eb6f4186d83c334c35e34b74d25aef
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2023-02-20 15:04:09 +0100

    import old regen from cake wars

--- src/main/java/me/verya/bedwars/Bedwars.java
+++ src/main/java/me/verya/bedwars/Bedwars.java
@@ -9,6 +9,7 @@ import xyz.nucleoid.plasmid.game.rule.GameRuleType;
 public class Bedwars implements ModInitializer {
     public static final String ID = "bedwars";
     public static final GameRuleType BED_INTERACTION = GameRuleType.create();
+    public static final GameRuleType SATURATED_REGENERATION = GameRuleType.create();
 
     @Override
     public void onInitialize() {
