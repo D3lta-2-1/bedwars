b2c2594a66bdb8c4d98b1a301fa1f1a4af92e76a
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2023-03-18 16:46:39 +0100

    make glass explosion proof

---
+++ src/main/java/fr/delta/bedwars/mixin/ExplosionBehaviorMixin.java
@@ -0,0 +1,47 @@
+package fr.delta.bedwars.mixin;
+
+import fr.delta.bedwars.Bedwars;
+import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.fluid.FluidState;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.BlockView;
+import net.minecraft.world.ServerWorldAccess;
+import net.minecraft.world.explosion.Explosion;
+import net.minecraft.world.explosion.ExplosionBehavior;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import xyz.nucleoid.plasmid.game.manager.GameSpaceManager;
+import xyz.nucleoid.plasmid.game.manager.ManagedGameSpace;
+
+import java.util.Optional;
+
+@Mixin(ExplosionBehavior.class)
+public class ExplosionBehaviorMixin {
+    private static final float GLASS_RESISTANCE = 99999.0F;
+
+    @Inject(method = "getBlastResistance", at = @At("HEAD"), cancellable = true)
+    private void getBlastResistance(
+            Explosion explosion, BlockView blockView,
+            BlockPos pos, BlockState block, FluidState fluid,
+            CallbackInfoReturnable<Optional<Float>> ci
+    ) {
+        if (blockView instanceof ServerWorldAccess) {
+            ServerWorld world = ((ServerWorldAccess) blockView).toServerWorld();
+
+            ManagedGameSpace gameSpace = GameSpaceManager.get().byWorld(world);
+            if (gameSpace != null) {
+                ActionResult result = gameSpace.getBehavior().testRule(Bedwars.BLAST_PROOF_GLASS_RULE);
+                if (result == ActionResult.SUCCESS) {
+                    if (block.getBlock() instanceof AbstractGlassBlock) {
+                        ci.setReturnValue(Optional.of(GLASS_RESISTANCE));
+                    }
+                }
+            }
+        }
+    }
+}
