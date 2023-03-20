ec5bbbb5f1d9101590681ccaae05b6e32c81f9ba
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2023-03-20 21:58:46 +0100

    refactor

--- src/main/java/fr/delta/bedwars/game/behaviour/WinEventSender.java
+++ src/main/java/fr/delta/bedwars/game/behaviour/WinEventSender.java
@@ -1,4 +1,4 @@
-package fr.delta.bedwars.game.behavior;
+package fr.delta.bedwars.game.behaviour;
 
 import fr.delta.bedwars.game.event.BedwarsEvents;
 import net.minecraft.entity.damage.DamageSource;
