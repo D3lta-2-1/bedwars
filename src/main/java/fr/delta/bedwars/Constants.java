f3461c5743fddd725fb0614d2f7305e5b816b1c0
d3lta <83219809+D3lta-2-1@users.noreply.github.com>
2023-04-03 18:12:17 +0200

    complete constants

--- src/main/java/fr/delta/bedwars/Constants.java
+++ src/main/java/fr/delta/bedwars/Constants.java
@@ -15,31 +15,57 @@ public class Constants {
     static public final List<DyeColor> TEAM_COLORS = new ArrayList<>(Arrays.asList(
             DyeColor.RED,
             DyeColor.BLUE,
-            DyeColor.GREEN,
             DyeColor.LIME,
             DyeColor.YELLOW,
-            DyeColor.ORANGE,
-            DyeColor.LIGHT_BLUE,
-            DyeColor.PURPLE,
             DyeColor.CYAN,
-            DyeColor.MAGENTA,
-            DyeColor.BROWN,
             DyeColor.WHITE,
             DyeColor.PINK,
             DyeColor.GRAY,
+
+            DyeColor.ORANGE,
             DyeColor.BLACK,
-            DyeColor.LIGHT_GRAY
+            DyeColor.PURPLE,
+            DyeColor.GREEN,
+
+            DyeColor.BROWN,
+            DyeColor.LIGHT_BLUE,
+            DyeColor.LIGHT_GRAY,
+            DyeColor.MAGENTA
+
     ));
     //usage ex: "blue_spawn" and "blue_bed"
     static public final String SPAWN = "spawn";
     static public final String BED = "bed";
     static public final String FORGE = "forge";
     static public final String ITEM_SHOPKEEPER = "item_shopkeeper";
+    static public final String TEAM_SHOPKEEPER = "team_shopkeeper";
 
     //TODO : must be completed with other flowers and fluids
     static public final List<Block> BreakableBlocks = new ArrayList<>(Arrays.asList(
             Blocks.TALL_GRASS,
             Blocks.WATER,
-            Blocks.POPPY)
+            Blocks.POPPY,
+            Blocks.FIRE,
+            Blocks.DANDELION,
+            Blocks.OXEYE_DAISY,
+            Blocks.CORNFLOWER,
+            Blocks.LILY_OF_THE_VALLEY,
+            Blocks.LILAC,
+            Blocks.ROSE_BUSH,
+            Blocks.SUNFLOWER,
+            Blocks.LARGE_FERN,
+            Blocks.TALL_SEAGRASS,
+            Blocks.SEAGRASS,
+            Blocks.BLUE_ORCHID,
+            Blocks.ALLIUM,
+            Blocks.AZURE_BLUET,
+            Blocks.RED_TULIP,
+            Blocks.ORANGE_TULIP,
+            Blocks.WHITE_TULIP,
+            Blocks.PINK_TULIP,
+            Blocks.PEONY,
+            Blocks.LILY_PAD,
+            Blocks.SUGAR_CANE,
+            Blocks.CACTUS)
     );
 }
